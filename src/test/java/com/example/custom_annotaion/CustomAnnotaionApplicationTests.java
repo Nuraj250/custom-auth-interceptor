6853a6ea0de3399641c71e75faae912abb5192ae
Nuraj250 <nurajshaminda200@gmail.com>
2025-01-16 22:57:07 -0600

    Initial commit for merging with existing repository

---
+++ src/test/java/com/example/custom_annotaion/CustomAnnotaionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.custom_annotaion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAnnotaionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
